0e42de1e70f504003d27b41ac110ce3637605430
unknown <akashrautela007@gmail.com>
2022-06-27 00:58:37 +0530

    initial commit

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://git.heroku.com/my-image-detection.git/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
